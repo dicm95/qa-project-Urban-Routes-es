b8249095cc0bbdfb2de7273f7d70468b71a93979
Daniela Contreras <muadcm@gmail.com>
2025-02-19 16:14:27 +0100

    Primer commit - Subiendo el proyecto

---
+++ README.md
@@ -0,0 +1,54 @@
+# Proyecto Urban Routes 
+
+### Contenido
+- Descripción
+- Lista de comprobación
+- Archivos del Proyecto
+- Recursos
+- Instrucciones para las pruebas
+
+---
+
+### Descripción
+
+En este proyecto probaremos la funcionalidad de la aplicación Urban Routes, primero, definiremos los localizadores y métodos necesarios y los almacenaremos en la clase UrbanRoutesPage, y segundo, construiremos pruebas automatizadas que cubran el proceso completo de pedir un taxi y las almacenaremos en la clase TestUrbanRoutes.
+
+---
+
+### Lista de pruebas
+
+| №    | Descripcion                                                                               
+|------|-------------------------------------------------------------------------------------------
+| 1    | **Configurar la dirección**                      
+| 2    | **Seleccionar la tarifa Comfort**
+| 3    | **Rellenar el número de teléfono**	 
+| 4    | **Agregar una tarjeta de crédito y hacer click en otro lugar de la pantalla** 
+| 5    | **Escribir un mensaje para el controlador**                   
+| 6    | **Pedir una manta y pañuelos**	                               
+| 7    | **Pedir 2 helados**	                                    
+| 8    | **Aparece el modal para buscar un taxi**	                              
+| 9    | **Esperar a que aparezca la información del conductor en el modal (opcional)**	     
+
+
+### Archivos del Proyecto
+
+- **data.py:** Este archivo contiene los cuerpos necesarios para las solicitudes. 
+- **main.py:** Este archivo contiene los loocalizadores, métodos y pruebas automatizadas.
+- **README.md:** Este archivo incluye una descripción del proyecto.
+
+---
+
+### Recursos
+
+**Paquetes instalados**
+- pytest
+- selenium
+
+---
+
+### Instrucciones 
+
+- Ejecuta todas las pruebas del proyecto a través de la terminal de PyCharm: escribe pytest main.py en la terminal.
+- Ejecuta todas las pruebas a través de la interfaz de PyCharm haciendo clic en el botón con un triángulo verde en la parte superior.
+  - Asegúrate de ejecutarlas en el archivo correcto, una forma sencilla de hacer esto es ir al archivo y seleccionar "Current File" antes de hacer clic en el botón con un triángulo verde.
+- Otra forma de ejecutar las pruebas es haciendo clic en las flechas verdes junto a las pruebas en el código.
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-
 # no modificar
 def retrieve_phone_code(driver) -> str:
     """Este código devuelve un número de confirmación de teléfono y lo devuelve como un string.
@@ -37,6 +36,18 @@ def retrieve_phone_code(driver) -> str:
 class UrbanRoutesPage:
     from_field = (By.ID, 'from')
     to_field = (By.ID, 'to')
+    comfort_tariff = (By.ID, 'tariff-card-4')
+    phone_field = (By.CLASS_NAME, 'np-button')
+    add_phone_number = (By.ID, 'phone')
+    payment_method = (By.CLASS_NAME, 'pp-button.filled')
+    add_card = (By_CLASS_NAME, 'pp-row.disabled')
+    card_number_field = (By.ID, 'number')
+    card_code_field = (By.ID, 'code')
+    card_wrapper = (By.CLASS_NAME, 'card-wrapper')
+    add_button = (By.XPATH, '//button[text()='Agregar']')
+    comment_field = (By.ID, 'comment')
+    toggle_switch = (By.CLASS_NAME, 'switch-input')
+    ice_cream_counter_plus = (By.XPATH, '//div[contains(text(), 'Helado')]//following::div[contains(@class, 'counter-plus')]')
 
     def __init__(self, driver):
         self.driver = driver
@@ -52,6 +63,42 @@ class UrbanRoutesPage:
 
     def get_to(self):
         return self.driver.find_element(*self.to_field).get_property('value')
+
+    def select_comfort(self):
+        self.driver.find_element(*self.comfort_tariff).click()
+
+    def click_phone_field(self):
+        self.driver.find_element(*self.phone_field).click()
+
+    def set_phone(self, phone_number):
+        self.driver.find_element(*self.add_phone_number).send_keys(phone_number)
+
+    def select_payment_method(self):
+        self.driver.find_element(*self.payment_method).click()
+
+    def add_payment_card(self):
+        self.driver.find_element(*self.add_card).click()
+
+    def add_card_number(self, card_number):
+        self.driver.find_element(*self.card_number_field).send_keys(card_number)
+
+    def add_cvv_code(self, card_code):
+        self.driver.find_element(*self.cvv_code_field).send_keys(card_code)
+
+    def activate_add_button(self):
+        self.driver.find_element(*self.card_wrapper).click()
+
+    def click_add_button(self):
+        self.driver.find_element(*self.add_button).click()
+
+    def add_message_for_driver(self, message_for_driver):
+        self.driver.find_element(*self.comment_field).send_keys(message_for_driver)
+
+    def add_blanket_and_scarves(self):
+        self.driver.find_element(*self.toggle_switch).click()
+
+    def add_ice_cream(self):
+        self.driver.find_element(*self.ice_cream_counter_plus).click()
 
 
 
@@ -76,6 +123,48 @@ class TestUrbanRoutes:
         assert routes_page.get_from() == address_from
         assert routes_page.get_to() == address_to
 
+    def test_set_comfort_tariff(self):
+        self.driver.get(data.urban_routes_url)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.select_comfort()
+
+    def test_set_phone_number(self):
+        self.driver.get(data.urban_routes_url)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.click_phone_field()
+        routes_page.set_phone(phone_number)
+        retrieve_phone_code(driver)
+
+    def test_add_payment_card(self):
+        self.driver.get(data.urban_routes_url)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.select_payment_method()
+        routes_page.add_payment_card()
+        routes_page.add_card_number(card_number)
+        routes_page.add_cvv_code(card_code)
+        routes_page.activate_add_button()
+        routes_page.click_add_button()
+
+    def test_add_driver_message(self):
+        self.driver.get(data.urban_routes_url)
+        routes_page = UrbanRoutesPage(self.driver)
+        message_for_driver = data.message_for_driver
+        routes_page.add_message_for_driver(message_for_driver)
+
+    def test_add_blanket_and_scarves(self):
+        self.driver.get(data.urban_routes_url)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.click_toggle()
+
+    def test_add_ice_cream(self):
+        self.driver.get(data.urban_routes_url)
+        routes_page = UrbanRoutesPage(self.driver)
+        routes_page.add_ice_cream()
+        routes_page.add_ice_cream()
+
+    def test_modal_appearance(self):
+        self.driver.get(data.urban_routes_url)
+        WebDriverWait(driver,10).until(expected_conditions.visibility_of_element_located((By.CLASS_NAME, 'smart-button'))
 
     @classmethod
     def teardown_class(cls):
